16396dae9049f3eb0743872cc03ed1f3781e88d8
Jirawat Boonkumnerd <jo06942@gmail.com>
2022-12-18 00:12:48 +0700

    feat: add capacitor

---
+++ android/app/src/main/java/dev/ntsd/crosshabit/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.ntsd.crosshabit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.ntsd.crosshabit',
+  appName: 'cross-habit',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
